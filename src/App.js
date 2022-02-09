10d3c9373e8466c618daf8a39ceee1d05a00c657
ankie17 <anutik26785@gmail.com>
2022-02-09 22:33:46 +0000

    server draft

--- src/App.js
+++ src/App.js
@@ -53,8 +53,11 @@ function App() {
 
       <br></br>
         <div>
-          <button>SUBMIT</button>
+          <button id="submit" onclick="addVenue(event);">SUBMIT </button> 
         </div>
+        {/* <div>
+        <ul id="venuelist"> </ul>
+        </div> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -67,5 +70,68 @@ function App() {
     </div>
   );
 }
+
+//comments by Anna for tomorrow
+/*
+1. i have added server with get and post request
+2. getvenueinfo fetching data
+3. addVenue added:
+4. reads values from fields added by Mundy and packs into json that is posts later
+5. submit button has now onclick
+6. prints venues list under submit button
+P.S. problems that might occur when uncomment:
+1. might need to change link localhost:3000 into .../get_venue_info or .../venue_info, later different one
+2. Note to Mundy: venuelist and everything else need to rendered accordingly to react rules, u did this in pure html
+3. i might need to change how date/time values are read from text fields
+
+/*const getVenueInfo = async () => {
+  const resp = await fetch('http://localhost:3000/get_venue_info')
+  return await resp.json()
+}
+
+
+const addVenue = async (e) => {
+  //ID!!!
+  const name = document.getElementById('venueName').value
+  //Photo!!!
+  const capacity = document.getElementById('venueCapacity').value
+  const address = document.getElementById('venueAddress').value
+  const startTime = document.getElementById('venueStartTime').value //syntax?
+  const endTime = document.getElementById('venueEndTime').value //syntax?
+  const email = document.getElementById('venueOwnerEmail').value
+  const startDate = document.getElementById('venueStartDate').value //syntax?
+  const endDate = document.getElementById('venueEndDate').value //syntax?
+
+
+  const info = JSON.stringify(
+      {
+          //venueID: ID,
+          venueName: name,
+          venueOwnerEmail: email,
+          venueCapacity: capacity,
+          venueAddress: address,
+          venueStartTime: startTime,
+          venueEndTime: endTime,
+          venueStartDate: startDate,
+          venueEndDate: endDate
+      }
+  )
+
+  await fetch('http://localhost:3000/venue_info', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: info
+  })
+
+  document.getElementById('venuelist').innerHTML = ''
+  getVenueInfo().then(
+      json => json.forEach(info => {
+          const listElement = document.createElement('li')
+          listElement.innerHTML = `Name: ${info.venueName} , Capacity: ${info.capacity} ,Location: ${info.venueGeoLocation}`
+          document.getElementById('venuelist').appendChild(listElement)
+      }
+      ))
+
+} */
 
 export default App;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+var cors = require('cors');
+server.use(cors())
+
+const venueInfo = []
+
+server.get('/get_venue_info', (req, res) => {
+    res.json(venueInfo)
+})
+
+server.post('/venue_info', (req, res) => {
+    if (typeof req.body != 'object' || req.body.venueID === undefined){ 
+        res.status(400)
+        res.send('Error')
+        return
+    } 
+    venueInfo.push(req.body)
+    console.log(req.body)
+    res.status(200)
+    res.send('ok')
+})
+
+server.listen(3000)
