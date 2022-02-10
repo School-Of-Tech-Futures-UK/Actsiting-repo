f41100caeaa6c29daf5bdb71a15891b3207859ef
CyntiBinti <cynthia_m@live.com>
2022-02-10 23:59:43 +0000

    downloaded material-UI for icons

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <img
+            className='header-icon'
+            src='https://cdn2.iconfinder.com/data/icons/location-map-simplicity/512/music_concert_hall-512.png'
+            alt='An icon of a location pointer'
+        />
+        </div>
+  )
+}
+
+export default Header
--- src/mainapp.js
+++ src/mainapp.js
@@ -5,13 +5,14 @@ other component gets passed through/re-routed through
 import React from 'react';
 import App from './App';
 import Homepage from './homepage';
+import Header from './header'
 
 
 // to change <App /> to instead be <AddVenuePage /> once page created
 function MainApp() {
     return (
         <>
-        <h1>Hello World from MainApp rendered!</h1>
+        <Header />
         <Homepage />
         </>
     )
