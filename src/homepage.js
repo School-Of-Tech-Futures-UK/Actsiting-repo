de633017c7ebef306d5365548c724ff260bfca6e
CyntiBinti <cynthia_m@live.com>
2022-02-10 09:30:43 +0000

    updated homepage file

---
+++ src/homepage.js
@@ -0,0 +1,36 @@
+//homepage component -- will pull in the AddVenuePage component as part of its hierarchy
+
+import React from 'react'
+
+function HomepageVenueImage() {
+    return (
+
+    )
+}
+
+
+function HomepageVenueDetails() {
+    return (
+        
+    )
+}
+
+function HomepageEditButton() {
+    return (
+        
+    )
+}
+
+function HomepageAddButton() {
+    return (
+        
+    )
+}
+
+function Homepage() {
+    return (
+
+    )
+}
+
+export default Homepage
