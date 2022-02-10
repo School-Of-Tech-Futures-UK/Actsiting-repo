58f15d36daf1253382960306d3424b7d8864b835
CyntiBinti <cynthia_m@live.com>
2022-02-10 10:44:53 +0000

    created usestate in homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,6 +1,12 @@
 //homepage component -- will pull in the AddVenuePage component as part of its hierarchy
 
+// AddVenuePage feeds into Homepage
+// Info populated from VenueSummaryPage updates state in Homepage "venue info" bit
+// from homepage wireframe
+
 import React from 'react'
+import { useState, useEffect } from "react";
+
 
 function HomepageVenueImage() {
     return (
@@ -28,6 +34,10 @@ function HomepageAddButton() {
 }
 
 function Homepage() {
+
+    const [editVenue, setEditVenue] = useState([]);
+    const [addVenue, setAddVenue] = useState()
+
     return (
 
     )
