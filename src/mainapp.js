b19302c458f1cb124979958baf0e2e89925b2b1c
CyntiBinti <cynthia_m@live.com>
2022-02-11 08:56:04 +0000

    created route paths

--- src/mainapp.js
+++ src/mainapp.js
@@ -5,17 +5,32 @@ other component gets passed through/re-routed through
 import React from 'react';
 import App from './App';
 import Homepage from './homepage';
-import Header from './header'
-import Footer from './footer'
+import Header from './header';
+import Footer from './footer';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
-// to change <App /> to instead be <AddVenuePage /> once page created
 function MainApp() {
     return (
         <>
-        <Header />
-        <Homepage />
-        <Footer />
+        <Router>
+            <Header />
+
+            <Switch>
+                {/* <Route path="/add-venue">
+                    <AddVenuePage />
+                </Route>
+                <Route path="/venue-summary">
+                    <VenueSummaryPage />
+                </Route> */}
+                <Route path="/">
+                    <Homepage />
+                </Route>
+            </Switch>
+
+
+            <Footer />
+        </ Router>
         </>
     )
 
