ffcb7de6e84bde3bdf78655c76b2fda732c8e323
CyntiBinti <cynthia_m@live.com>
2022-02-11 08:42:03 +0000

    updated footer

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './footer.css'
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Footer() {
   return (
     <div className="homepage-footer">
-        <p>(c)2022 Actsiting (part of GigStr SOTF)</p>
-        <p>Check out our <a href="https://github.com/School-Of-Tech-Futures-UK/Actsiting-repo">Github</a>
+        <p>©2022 Actsiting (part of GigStr SOTF)</p>
+        <p>Check out our <a href="https://github.com/School-Of-Tech-Futures-UK/Actsiting-repo">Github
+        <GitHubIcon /></a>
 </p>
     </div>
   )
