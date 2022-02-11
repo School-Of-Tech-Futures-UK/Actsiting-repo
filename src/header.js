ee227416fcf99a028c583bdb5a1afe69f20ca4ef
CyntiBinti <cynthia_m@live.com>
2022-02-11 01:15:37 +0000

    header section done

--- src/header.js
+++ src/header.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import './header.css'
+import SearchIcon from "@material-ui/icons/Search"
+import { Avatar } from "@material-ui/core"
 
 function Header() {
   return (
     <div className='header'>
+
         <img
             className='header-icon'
             src='https://cdn2.iconfinder.com/data/icons/location-map-simplicity/512/music_concert_hall-512.png'
             alt='An icon of a location pointer'
         />
+
+        <div className='header-search-bar'>
+          <input type="text" />
+          <SearchIcon />
         </div>
+
+        <div className='header-right-icons'>
+          <p>Login / sign up</p>
+          <Avatar />
+        </div>
+
+      </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MainApp from './mainapp';
+import MainApp from './mainApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
