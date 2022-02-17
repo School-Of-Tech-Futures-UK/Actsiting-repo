3c6f6d9a83d6b6d2dd94f753e7a20afac512cbcf
harri853 <59712756+harri853@users.noreply.github.com>
2022-02-17 16:56:26 +0000

    Update setup-tables.sql

--- database/setup-tables.sql
+++ database/setup-tables.sql
@@ -3,3 +3,13 @@ CREATE TABLE Listed_Venues (
    venue_name VARCHAR(255) NOT NULL,
    PRIMARY KEY(venue_id)
 );
+
+CREATE TABLE Listed_Venues (
+   venue_id INTEGER GENERATED ALWAYS AS IDENTITY,
+   venue_name VARCHAR(255) NOT NULL,
+   PRIMARY KEY(venue_id)
+);
+
+SELECT * FROM Listed_Venues
+
+INSERT INTO Listed_Venues(venue_name) VALUES ('London')
