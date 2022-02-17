369ee544bf73398d89b3cb1c86ca4bcacd86544f
Harri Prasad <harri.prasad@accenture.com>
2022-02-17 15:10:25 +0000

    trying to make db work

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,45 @@ server.use(express.json());
 var cors = require('cors');
 const { application } = require('express');
 server.use(cors())
+const Postgres = require('pg').Postgres
 //const serverlessExpress = require('@vendia/serverless-express')
 
+const dbServer = process.env.DB_HOSTNAME || '127.0.0.1'
+const dbPassword = process.env.DB_PASSWORD
+
+
+const db = new Postgres({
+    host: dbServer,
+    database: 'postgres',
+    user: 'postgres',
+    password: dbPassword,
+    port: 5432,
+})
 
 const venueInfo = []
 
-server.get('/venue_info', (req, res) => {
-    res.json(venueInfo)
+
+
+server.get('/venue_info', async (req, res) => {
+    const storeVenue = await db.query(`SELECT venue_id, venue_name FROM Listed_Venues where venue_id=${req.params.id}`)
+    res.send(storeVenue)
+    // res.json(venueInfo)
 })
 
+// insert user input field data as new info in the database
 server.post('/venue_info', (req, res) => {
    /* if (typeof req.body != 'object' || req.body.venueID === undefined){ 
         res.status(400)
         res.send('Error')
         return
     } */ 
-    venueInfo.push(req.body)
+    const { venue_id, venue_name } = req.body.venue
+    
+    db.query(
+        'INSERT INTO Listed_Venues (venue_id, venue_name)',
+        [venue_id, venue_name]
+    )
+    //venueInfo.push(req.body)
     console.log(req.body)
     res.status(200)
     return res.send('ok')
