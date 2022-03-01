600e1bc55af7e269693d8effc3aaaf61be01ff2d
CyntiBinti <cynthia_m@live.com>
2022-03-01 18:24:42 +0000

    events API fetching

--- frontend/src/addVenuePage.js
+++ frontend/src/addVenuePage.js
@@ -22,6 +22,11 @@ function AddVenuePage() {
   const inputRefStartDate = useRef(null)
   const [venueEndDate, setVenueEndDate] = useState([])
   const inputRefEndDate = useRef(null)
+  const [venueImage, setVenueImage] = useState([])
+  const inputRefImage = useRef(null)
+  const [venueDescription, setVenueDescription] = useState([])
+  const inputRefDescription = useRef(null) 
+
 
   const PostVenueName = async (e) => {
     console.log(inputRefName)
@@ -33,7 +38,9 @@ function AddVenuePage() {
             geolocation: inputRefGeoLocation.current.value,
             email: inputRefOwnerEmail.current.value,
             startDate: inputRefStartDate.current.value,
-            endDate: inputRefEndDate.current.value
+            endDate: inputRefEndDate.current.value,
+            image: inputRefImage.current.value,
+            description: inputRefDescription.current.value
         }
     )
 
@@ -54,6 +61,8 @@ function AddVenuePage() {
     setVenueOwnerEmail(json)
     setVenueStartDate(json)
     setVenueEndDate(json)
+    setVenueImage(json) //set venue image as json for now?
+    setVenueDescription(json)
   }
   
 
@@ -70,6 +79,10 @@ function AddVenuePage() {
       <input ref = {inputRefStartDate} type="date" id="venueStartDate" placeholder="venueStartDate"></input>
       <input ref = {inputRefEndDate} type="date" id="venueEndDate" placeholder="venueEndDate"></input>
       </div>
+      <div>
+      <input ref = {inputRefImage} type="text" id="venueImage" placeholder="venueImage"></input>
+      <input ref = {inputRefDescription} type="text" id="venueDescription" placeholder="venueDescription"></input>
+      </div>
 
       {/* <Link to='/'> */}
         <button id="submit" onClick={PostVenueName}>SUBMIT</button> 
@@ -79,11 +92,11 @@ function AddVenuePage() {
       
 
       
-      <p>{venueName.map(place => <p>{place.venue_id} , {place.venue_name}, {place.venue_capacity} , {place.venue_address} , {place.venue_geolocation} , {place.venue_owner_email} , {place.venue_start_date} , {place.venue_end_date}</p>)}</p>
+      <p>{venueName.map(place => <p>{place.venue_id} , {place.venue_name}, {place.venue_capacity} , {place.venue_address} , {place.venue_geolocation} , {place.venue_owner_email} , {place.venue_start_date} , {place.venue_end_date}, {place.venue_image}, {place.venue.description}</p>)}</p>
       {/* <p>{venueCapacity.map(place => <p>{place.venue_id} , {place.venue_capacity}</p>)}</p>
       <p>{venueAddress.map(place => <p>{place.venue_id} , {place.venue_address}</p>)}</p>
       <p>{venueGeoLocation.map(place => <p>{place.venue_id} , {place.venue_geolocation}</p>)}</p> */}
-      
+
     </div>
   )
 }
